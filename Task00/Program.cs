7bd4e61b0ea6160becf802b916e09a3f085a8e3d
Alexander <continenttal2015@yandex.ru>
2023-03-05 10:31:19 +0300

    Completed task00

---
+++ Task00/Program.cs
@@ -0,0 +1,5 @@
+﻿// 0. Вывести квадрат числа
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+double numberSquared=Math.Pow(number, 2);
+Console.WriteLine($"Квадрат числа {number} -> {numberSquared}");
