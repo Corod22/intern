e278128e794a988256b56f422e2c5486a07f548c
Alexander <continenttal2015@yandex.ru>
2023-03-05 14:46:04 +0300

    Completed task03

---
+++ Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿// 3. По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите номер дня недели");
+int weekDay=Convert.ToInt32(Console.ReadLine());
+if (weekDay>0 && weekDay<8)
+{
+    if (weekDay==1) Console.WriteLine("Понедельник");
+    else if (weekDay==2) Console.WriteLine("Вторник");
+    else if (weekDay==3) Console.WriteLine("Среда");
+    else if (weekDay==4) Console.WriteLine("Четверг");
+    else if (weekDay==5) Console.WriteLine("Пятница");
+    else if (weekDay==6) Console.WriteLine("Суббота");
+    else if (weekDay==7) Console.WriteLine("Воскресенье");
+}
+else
+{
+ Console.WriteLine("Некорректно ввели номер дня недели");
+}
+
+Console.Read();
