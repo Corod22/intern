29fd54d605fa0037232555c7723c8100c39dfcbf
Alexander <continenttal2015@yandex.ru>
2023-03-07 14:17:48 +0300

    Completed task12

---
+++ Task12/Program.cs
@@ -0,0 +1,7 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное число");
+int threeDigitNumber=Convert.ToInt32(Console.ReadLine());
+int num1=threeDigitNumber%10;
+int num2=threeDigitNumber/100;
+int twoDigitNumber=num2*10+num1;
+Console.WriteLine($"Двухзначное число {twoDigitNumber}");
