54abd8ae2dcaffa063c67c859222c7b3240951ec
Alexander <continenttal2015@yandex.ru>
2023-03-07 13:38:03 +0300

    Completed task07

---
+++ Task07/Program.cs
@@ -0,0 +1,8 @@
+﻿// 7. Показать числа от -N до N
+Console.WriteLine("Введите число N");
+int numberN=Convert.ToInt32(Console.ReadLine());
+for (int i = -numberN; i <= numberN ; i++)
+{
+    Console.Write($"{i} ");
+}
+Console.Read();
