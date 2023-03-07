8134d2b80899fe0fcac2b180e4a13df54f60a036
Alexander <continenttal2015@yandex.ru>
2023-03-07 14:01:26 +0300

    Выполнено задание10

---
+++ Task10/Program.cs
@@ -0,0 +1,4 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное число");
+int threeDigitNumber=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вторая цифра числа {threeDigitNumber}->{threeDigitNumber/10%10}");
