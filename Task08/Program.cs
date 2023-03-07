3d21ad08fa2e1ee8a8d721cb7a63a17ceb33c712
Alexander <continenttal2015@yandex.ru>
2023-03-07 13:45:57 +0300

    Completed task08

---
+++ Task08/Program.cs
@@ -0,0 +1,8 @@
+﻿// 8. Показать четные числа от 1 до N
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Положительные числа:");
+for (int i = 1; i <= numberN; i++)
+{
+    if (i % 2 == 0) Console.Write($" {i}");
+}
