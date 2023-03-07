31043759672d343a59b0e051b7f2134b7f6e0bae
Alexander <continenttal2015@yandex.ru>
2023-03-07 13:54:59 +0300

    Completed task09

---
+++ Task09/Program.cs
@@ -0,0 +1,4 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное чисо");
+int threeDigitNumber=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Последняя цифра числа {threeDigitNumber}-> {threeDigitNumber%10}");
