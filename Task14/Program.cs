6d65a9ed8ba2e763ae4edd521cb521e0b79094da
Alexander <continenttal2015@yandex.ru>
2023-03-07 15:01:09 +0300

    Completed task14

---
+++ Task14/Program.cs
@@ -0,0 +1,14 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>100)
+{
+    int num=number;
+while(num>1000)
+{
+num=num/10;
+}
+int threeDigitNumber = num%10;
+Console.WriteLine($"Третья цифра числа {threeDigitNumber}");
+}
+else Console.WriteLine("Третьей цифры нет");
