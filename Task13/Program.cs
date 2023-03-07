b78c3964ebc2ca1fc932668c0702f18ebcafbbe3
Alexander <continenttal2015@yandex.ru>
2023-03-07 14:29:00 +0300

    Completed task13

---
+++ Task13/Program.cs
@@ -0,0 +1,6 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число проверки");
+int numberCheck = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите заданное число");
+int givenNumber=Convert.ToInt32(Console.ReadLine());
+if (numberCheck%givenNumber!=0) Console.WriteLine($"Остаток числа {numberCheck%givenNumber}");
